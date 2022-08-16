9237acfaccb7af12a7aab1a51d64f4e164cf92db
1g0rbm <m1g0rb89@gmail.com>
2022-08-16 21:27:47 +0300

    Moved terraform backend settings to backend.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,18 +11,6 @@ terraform {
   }
 
   required_version = ">= 0.13"
-
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "1g0rbm-terraform-storage"
-    region     = "ru-central1"
-    key        = "terraform-backend/state.tfstate"
-    # access_key
-    # secret_key
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
 }
 
 variable "token" {
